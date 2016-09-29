91662b53c85f017a37c2afbf049417dbf30c513f
multist <multist.hh@gmail.com>
2016-09-29 11:36:57 +0900

    test

---
+++ 20160929/hoon.py
@@ -0,0 +1,16 @@
+# coding=utf-8
+
+print('구구단 만들기')
+
+for i in range(2,9):
+    for j in range(2,9):
+        print("{} X {} = {}", format(i, j, i*j))
+
+
+
+
+# www.pythonchallenge.com
+# 코드 레벨업
+
+# www.codility.com
+# 코드 능력 체크
