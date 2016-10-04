2651f8191acf09fa556b0c9889a15866770ee8a4
multist <multist.hh@gmail.com>
2016-10-04 11:20:28 +0900

    test

--- 20160930/hoon.py
+++ 20160930/hoon.py
@@ -1,6 +1,12 @@
-# 주어진 문장
-txt = "g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj."
+# 문제 URL : http://www.pythonchallenge.com/pc/def/map.html
 
+import string
+
+# 주어진 문장
+#txt = "g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj."
+
+# URL을 치환함
+txt = "map"
 # 변환 후 문자열
 result = "result : "
 
@@ -42,4 +48,6 @@ for idx in range(0, cnt):
 
 print(result)
 # 결과 : i hope you didnt translate it by hand. thats what computers are for. doing it in by hand is inefficient and that's why this text is so long. using string.maketrans() is recommended. now apply on the url.
+
+# 결과 : ocr
 
