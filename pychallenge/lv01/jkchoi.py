c998100cb27515b8bbf51fcbd3c172288fc60867
jkchoi <arsea.choi@gmail.com>
2016-10-05 11:22:09 +0900

    파일 정리

---
+++ pychallenge/lv01/jkchoi.py
@@ -0,0 +1,23 @@
+
+# only small case
+data = "g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj."
+
+result = ""
+for i in range(0, len(data)):
+    c = ord(data[i])
+    if 97 <= c <= 127:
+        c +=2
+        if ( c >  122 ):
+            c -= (ord("z") - ord("a")+1)
+
+    result += chr(c)
+
+
+print("original : " + data)
+print("result : " + result)
+
+orimap = "abcdefghijklmnopqrstuvwxyz"
+newmap = "cdefghijklmnopqrstuvwxyzab"
+print(data.translate(str.maketrans(orimap,newmap)))
+
+print("map".translate(str.maketrans(orimap,newmap)))
