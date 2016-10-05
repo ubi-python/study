2aa6637848172f1ffd5bb87a6df6c00053b616b4
nightcat <nightcat@gmail.com>
2016-10-05 11:11:51 +0900

    [2016.10.05]권병수 - lv00~02

---
+++ pychallenge/lv01/byungsu.py
@@ -0,0 +1,13 @@
+# coding:UTF-8
+from string import maketrans   # Required to call maketrans function.
+
+# 2번 문제
+print("g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj.".rsplit(' '))
+str = "g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj."
+string = "g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj.".rsplit(' ')[::1]
+url = 'map'
+intab = 'abcdefghijklmnopqrstuvwxyz'
+outtab = 'cdefghijklmnopqrstuvwxyzab'
+transtab = maketrans(intab, outtab)
+print url.translate(transtab)
+
