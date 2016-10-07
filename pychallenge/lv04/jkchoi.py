60556bc32f7a6c88f89767e387c8b51b18ba2ecd
jkchoi <arsea.choi@gmail.com>
2016-10-07 13:30:57 +0900

    lv04 수행

--- pychallenge/lv04/jkchoi.py
+++ pychallenge/lv04/jkchoi.py
@@ -1,12 +1,24 @@
 import urllib.request
 import collections
 
+baseurl = "http://www.pythonchallenge.com/pc/def/linkedlist.php?nothing="
 
-def getContent(no):
-    content = urllib.request.urlopen("http://www.pythonchallenge.com/pc/def/linkedlist.php?nothing="+no).read()
-    text = content.decode("utf-8")
+def getContent(_no):
+    print("try : " + _no)
+    content = urllib.request.urlopen(baseurl + _no).read()
+    return content.decode("utf-8")
 
 
+prefix = "and the next nothing is "
+no = "12345"
+while True :
+    text = getContent(no)
+    print("response : " + text)
+    if prefix in text:
+        no = text[text.find(prefix) + len(prefix):]
+    elif "Yes. Divide by two and keep going." in text:
+        no = str( int(int(no) / 2)  )
+    else:
+        break
 
-
-print(getContent("12345"))
+# result : http://www.pythonchallenge.com/pc/def/peak.html
