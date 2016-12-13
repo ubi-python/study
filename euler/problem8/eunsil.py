a37b8bacea9780bcace348d9ad1f20161b678735
choieunsil <eunsil1990@gmail.com>
2016-12-13 13:36:53 +0900

    euler 레벨8. 문제해결. 다른 방법으로 접근중

--- euler/problem8/eunsil.py
+++ euler/problem8/eunsil.py
@@ -27,3 +27,62 @@
 이런 식으로 맨 처음 (7 × 3 × 1 × 6 × 7 = 882) 부터 맨 끝 (6 × 3 × 4 × 5 × 0 = 0) 까지 5자리 숫자들의 곱을 구할 수 있습니다.
 이렇게 구할 수 있는 5자리 숫자의 곱 중에서 가장 큰 값은 얼마입니까?
 """
+
+data =  str(7316717653133062491922511967442657474235534919493496983520312774506326239578318016984801869478851843858615607891129494954595017379583319528532088055111254069874715852386305071569329096329522744304355766896648950445244523161731856403098711121722383113622298934233803081353362766142828064444866452387493035890729629049156044077239071381051585930796086670172427121883998797908792274921901699720888093776657273330010533678812202354218097512545405947522435258490771167055601360483958644670632441572215539753697817977846174064955149290862569321978468622482839722413756570560574902614079729686524145351004748216637048440319989000889524345065854122758866688116427171479924442928230863465674813919123162824586178664583591245665294765456828489128831426076900422421902267105562632111110937054421750694165896040807198403850962455444362981230987879927244284909188845801561660979191338754992005240636899125607176060588611646710940507754100225698315520005593572972571636269561882670428252483600823257530420752963450)
+first=[]
+
+def compareNum(first):
+    for i in range( 0, len(data)-4):
+        subStr = list(data [i:i+5])
+        remainNum = []
+        if( i == 0):
+            first= subStr[0:]
+            continue
+
+        tempFirst = first[0:]
+        for x in subStr:
+            if( x in tempFirst):
+                tempFirst.remove(x)
+            else:
+                remainNum.append(x)
+        firstMut =1
+        secondMut = 1
+        for x in tempFirst:
+            firstMut *= int(x)
+        for x in remainNum:
+            secondMut *= int(x)
+        if( secondMut > firstMut):
+            first = subStr
+    return first
+
+def nogada(first):
+    for i in range( 0, len(data)-4):
+        subStr = list(data [i:i+5])
+        remainNum = []
+        if( i == 0):
+            first= subStr[0:]
+            continue
+
+        tempFirst = first[0:]
+        for x in subStr:
+            if( x in tempFirst):
+                tempFirst.remove(x)
+            else:
+                remainNum.append(x)
+        firstMut =1
+        secondMut = 1
+        for x in tempFirst:
+            firstMut *= int(x)
+        for x in remainNum:
+            secondMut *= int(x)
+        if( secondMut > firstMut):
+            first = subStr
+    return first
+
+#first=compareNum(first)
+first=nogada(first)
+print(first)
+result = 1
+for n in first:
+    result*= int(n)
+print(result)
