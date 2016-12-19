6b0da1bc29c1253465aeca4ef41eb65b2024d552
ggonie <crom0406@gmail.com>
2016-12-19 12:58:07 +0900

    문제 7번 완료

---
+++ euler/problem7/sanggon.py
@@ -0,0 +1,45 @@
+# q7
+
+'''
+소수를 크기 순으로 나열하면 2, 3, 5, 7, 11, 13, ... 과 같이 됩니다.
+이 때 10,001번째의 소수를 구하세요.
+'''
+
+base_prime_num = list()
+base_prime_num.append(2)
+
+
+def is_divide_by_prime_num(num):
+    for x in base_prime_num:
+        if num % x == 0:
+            return True
+    return False
+
+
+def is_prime_num(num, base):
+    while base != num:
+        if num % base == 0:
+            return False
+        else:
+            base += 1
+    return True
+
+
+def count_prime_num(start, question):
+    count = 1
+    max_prime_num = 1
+    while True:
+        if not is_divide_by_prime_num(start):
+            if is_prime_num(start, max_prime_num + 1):
+                count += 1
+                max_prime_num = start
+                base_prime_num.append(max_prime_num)
+                #                print("소수 개수 : ", count, ", max : ", max_prime_num)
+        start += 1
+        if count == question:
+            break
+    return base_prime_num[-1]
+
+
+answer = count_prime_num(2, 10001)
+print("답 : {}".format(answer))
