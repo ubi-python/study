7d74706e372195bee792eaf5fb3b8b1ddfb03873
ggonie <crom0406@gmail.com>
2017-01-09 12:30:17 +0900

    문제 9번 Complete

--- euler/problem9/sanggon.py
+++ euler/problem9/sanggon.py
@@ -5,13 +5,13 @@ import math
 pow_num_list = list()
 idx = 1
 flag = False
-while (flag != True):
+while flag != True:
     c_powed = math.pow(idx, 2)
     idx += 1
     if len(pow_num_list) < 3:
         pow_num_list.append(c_powed)
         continue
-    # b_powed = pow_num_list[-1]
+
     for b_powed in pow_num_list:
         a_powed = c_powed - b_powed
 
@@ -24,7 +24,5 @@ while (flag != True):
                 print(a * b * c)
                 flag = True
                 break
-            elif 1000 < (a + b + c):
-                print('Not Found')
-                break
-    pow_num_list.append(c_powed)
+
+    pow_num_list.insert(0, c_powed)
